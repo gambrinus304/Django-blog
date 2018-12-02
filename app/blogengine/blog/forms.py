5dc59c095a10d61d18be658762c03a9c7cb00078
gambrinus304 <devilhunter2009@mail.ru>
2018-12-03 01:37:58 +0300

    ...

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -4,14 +4,20 @@ from django.core.exceptions import ValidationError
 
 
 
+class TagForm(forms.ModelForm):
+    # title = forms.CharField(max_length=50)
+    # slug = forms.CharField(max_length=50)
 
+    # title.widget.attrs.update({'class': 'form-control'})
+    # slug.widget.attrs.update({'class': 'form-control'})
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
 
-class TagForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    slug = forms.CharField(max_length=50)
-
-    title.widget.attrs.update({'class': 'form-control'})
-    slug.widget.attrs.update({'class': 'form-control'})
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 
     def clean_slug(self):
@@ -19,12 +25,14 @@ class TagForm(forms.Form):
 
         if new_slug == 'create':
             raise ValidationError('Slug may not be "Create"')
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError('Slug must be unique. We hawe "{}" slug already'.format(new_slug))
         return new_slug
 
 
-    def save(self):
-        new_tag = Tag.objects.create(
-            title=self.cleaned_data['title'], 
-            slug=self.cleaned_data['slug'])
-        return new_tag
+    # def save(self):
+    #     new_tag = Tag.objects.create(
+    #         title=self.cleaned_data['title'], 
+    #         slug=self.cleaned_data['slug'])
+    #     return new_tag
 
